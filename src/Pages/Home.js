2d99900a20c888af9bb67166dbccaa8e8a55baca
HaJunRyu <hjr4557@naver.com>
2021-10-12 05:55:20 +0900

    Feat: 페이지 첫 진입시 /home 라우트로 이동하게끔 설정
    라우트는 상수로 따로 관리하게끔 파일 및 변수 추가
    
    Changes to be committed:
    modified:   src/Pages/Home.js
    modified:   src/Routes.js
    new file:   src/constant/index.js
    new file:   src/constant/route.js

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
     <div>
-      
+      <Link to="/beerlist">beer more</Link>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- src/Routes.js
+++ src/Routes.js
@@ -1,19 +1,16 @@
-import React from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-} from "react-router-dom";
-import Home from "./Pages/Home";
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import { HOME_PAGE } from './constant';
+import Home from './Pages/Home';
 // import ReactGA from "react-ga";
 
 const Routes = () => {
-
   return (
     <Router>
-        <Switch>
-          <Route path="/home" component={Home} />
-        </Switch>
+      <Switch>
+        <Route path={HOME_PAGE} component={Home} />
+        <Redirect to={HOME_PAGE} />
+      </Switch>
     </Router>
   );
 };
